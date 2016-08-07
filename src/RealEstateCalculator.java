21b88473f3b0f16317b0117d3e131aeceac83413
genericlady <yiannakokalas@gmail.com>
2016-08-07 09:15:20 -0400

    new directories

---
+++ src/RealEstateCalculator.java
@@ -0,0 +1,6 @@
+class RealEstateCalculator {
+  public RealEstateCalculator() {
+
+  }
+
+}
---
+++ src/RealEstateCalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class RealEstateCalculatorTest {
+
+  @Before public void setup() {
+    RealEstateCalculator realEstateCalculator = new RealEstateCalculator();
+  }
+}
