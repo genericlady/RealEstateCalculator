ec33ef62ed58ac0ce45226c04fc6fa9140a5d92e
genericlady <yiannakokalas@gmail.com>
2016-08-09 21:16:21 -0400

    decoupled tests and added Territory object

--- src/main/java/RealEstate.java
+++ src/main/java/RealEstate.java
@@ -5,6 +5,10 @@ class RealEstate {
   private BigDecimal improvementValue = new BigDecimal(0);
   private String stateName;
 
+  private County county;
+  private City city;
+  private SchoolDistrict schoolDistrict;
+
   public RealEstate(BigDecimal landValue, BigDecimal improvementValue, String stateName) {
     this.landValue = landValue;
     this.improvementValue = improvementValue;
@@ -14,4 +18,17 @@ class RealEstate {
   public BigDecimal totalValue() {
     return landValue.add(improvementValue);
   }
+
+  public void setCounty(County county) {
+    this.county = county;
+  }
+
+  public void setCity(City city) {
+    this.city = city;
+  }
+
+  public void setSchoolDistrict(SchoolDistrict schoolDistrict) {
+    this.schoolDistrict = schoolDistrict;
+  }
+
 }
--- src/test/java/RealEstateTest.java
+++ src/test/java/RealEstateTest.java
@@ -9,28 +9,39 @@ public class RealEstateTest {
   String stateName = "New York";
   RealEstate realEstate = new RealEstate(landValue, improvementValue, stateName);
 
+  County county = new County('Queens County', 3);
+  City city = new City('Astoria', 2);
+  SchoolDistrict schoolDistrict = new SchoolDistrict('District #30', 1);
+  
+  realEstate.setCounty(county);
+  realEstate.setCity(city);
+  schoolDistrict.setSchoolDistrict(schoolDistrict);
+
   @Test public void totalRealEstateValue() {
     assertEquals(new BigDecimal(750000), realEstate.totalValue());
   }
 
-  @Test public void hasCountyNameAndTaxRate() {
-    County county = new County('Queens County', 3);
-    realEstate.setCounty(county);
+  @Test public void hasCountyName() {
     assertEquals('Queens County', realEstate.getCountyName());
+  }
+
+  @Test public void hasCountyTaxRate() {
     assertEquals(3, realEstate.getCountyTaxRate());
   }
 
-  @Test public void hasCityNameAndTaxRate() {
-    City city = new City('Astoria', 2);
-    realEstate.setCity(city);
+  @Test public void hasCityName() {
     assertEquals('Astoria', realEstate.getCityName());
+  }
+
+  @Test public void hasCityTaxRate() {
     assertEquals(2, realEstate.getCityTaxRate());
   }
 
-  @Test public void hasSchoolDistrictNameAndTaxRate() {
-    SchoolDistrict schoolDistrict = new SchoolDistrict('District #30', 1);
-    schoolDistrict.setSchoolDistrict(schoolDistrict);
+  @Test public void hasSchoolDistrictName() {
     assertEquals('District #30', schoolDistrict.getSchoolDistrictName());
+  }
+
+  @Test public void hasSchoolDistrictTaxRate() {
     assertEquals(1, schoolDistrict.getSchoolDistrictTaxRate());
   }
 }
